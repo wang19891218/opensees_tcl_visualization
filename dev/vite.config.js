aa390bf6630e23bce6c4427761a27f2383b08e27
wang19891218 <wang19891218@gmail.com>
2021-12-09 21:36:48 +0000

    bug fix, for github page display

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,6 +1,8 @@
 export default {
     "server": { "host": "0.0.0.0" },
+    "base": "https://wang19891218.github.io/opensees_tcl_visualization/",
     "build": {
         "outDir": "../dist",
+        "assetsDir": "assets",
         "emptyOutDir": true}
 }
