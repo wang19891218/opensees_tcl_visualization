87d246f4fee7136035934dbcc44ae4fe6755b68e
wang19891218 <wang19891218@gmail.com>
2022-01-24 05:07:03 +0000

    test

--- scripts/push_subtree_to_github_page.sh
+++ scripts/push_subtree_to_github_page.sh
@@ -1,1 +1,5 @@
- git subtree push --prefix dist origin gh-pages
+echo openseestclvisualization.app.hfwang.dev > dist/CNAME
+git add dist
+git commit -m "add CNAME to dist" 
+git subtree push --prefix dist origin gh-pages
+
